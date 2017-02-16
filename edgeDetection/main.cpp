badc7375327b1a8417f6f95eaf4400cfc5442630
Robert Artin <Robert.Artin@endava.com>
2017-02-16 16:56:55 +0200

    Added edge detection algorithm

--- edgeDetection/main.cpp
+++ edgeDetection/main.cpp
@@ -1,38 +1,77 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <iostream>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
 
+//Global variables
+
+Mat src , src_gray;
+Mat dst,detected_edges;
+
+int edgeThresh=1;
+int lowThreshold;
+int const max_lowThreshold=100;
+int ratio=3;
+int kernel_size=3;
+char* window_name="Edge Map";
+
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+void CannyThreshold(int, void*)
+{
+    /// Reduce noise with a kernel 3x3
+    blur( src_gray, detected_edges, Size(3,3) );
+
+    /// Canny detector
+    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+    /// Using Canny's output as a mask, we display our result
+    dst = Scalar::all(0);
+
+    src.copyTo( dst, detected_edges);
+    imshow( window_name, dst );
+}
+
+
 int main(int argc, char** argv)
 {
 
-    String imageName("../header.jpg");
-
-//    if(argc != 2)
-//    {
-//        std::cout << "Usage: display_image ImageToLoadAndDisplay" << std::endl;
-//        return -1;
-//    }
+    String imageName("../chess.jpg");
 
     if(argc >1){
         imageName=argv[1];
     }
 
-    Mat frame;
-    frame = imread(imageName, CV_LOAD_IMAGE_COLOR); // Read the file
+    src = imread(imageName); // Read the file
 
-    if(!frame.data) // Check for invalid input
+    if(!src.data) // Check for invalid input
     {
         std::cout << "Could not open or find the frame" << std::endl;
         return -1;
     }
 
-    namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Window", frame); // Show our image inside it.
+    //Create a matrix of the same type and size as src (for dst)
+    dst.create(src.size(),src.type());
 
+    //Comvert the image to grayscale
+    cv::cvtColor(src,src_gray,CV_BGR2GRAY);
+
+    //Create a window
+    namedWindow(window_name,CV_WINDOW_AUTOSIZE);
+
+    //Create a Trackbar for user to enter threshold
+    createTrackbar("Min Threshold:",window_name,&lowThreshold,max_lowThreshold,CannyThreshold);
+
+    //Show the image
+    CannyThreshold(0,0);
+
+    //Wait until user exit program by pressing a key
     cv::waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
