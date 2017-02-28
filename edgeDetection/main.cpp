6b87bdb345096d2b7a93c6876e44cdc56b98bbc4
Robert Artin <Robert.Artin@endava.com>
2017-02-28 18:35:29 +0200

    Tried to find 2 pairs of censcutive lines and find a square but the duplicates are ruining it

--- edgeDetection/main.cpp
+++ edgeDetection/main.cpp
@@ -2,7 +2,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <io.h>
-#include <set>
 
 #include "IPM.h"
 #include "imagePreparation.h"
@@ -127,7 +126,7 @@ void CannyThreshold(int, void *) {
  * @param pointY
  * @param iteration
  */
-void putTextIntoImage(const char *pathdir,cv::Mat &src, int pointNr, int pointX, int pointY, int iteration) {
+void putTextIntoImage(const char *pathdir, cv::Mat &src, int pointNr, int pointX, int pointY, int iteration) {
     int check = mkdir(pathdir);
     int fontFace = FONT_HERSHEY_SIMPLEX;
     double fontScale = 2;
@@ -136,28 +135,68 @@ void putTextIntoImage(const char *pathdir,cv::Mat &src, int pointNr, int pointX,
     sprintf(s, "(%d) %d %d", pointNr, pointX, pointY);
     putText(src, s, Point(pointX, pointY), fontFace, fontScale, Scalar(0, 0, 255), thickness, 8, false);
     char path[50];
-    sprintf(path,"%s/grey_iteration_%d.jpg",pathdir,iteration);
+    sprintf(path, "%s/grey_iteration_%d.jpg", pathdir, iteration);
     imwrite(path, src);
 }
 
 /**
- * This function is used in the sort() call so we can sort the vector containing the lines after the X of the first point
+ * This function is used in the sort() call so we can sort ASCENDING the vector containing the lines after the X of the first point
  * @param v1 The first line
  * @param v2 The second line
  * @return returns if the X of the first point is smaller than the second lines first point X
  */
-bool comparisonFirstPointX(Vec4i v1, Vec4i v2) {
+bool comparisonFirstPointXAsc(Vec4i v1, Vec4i v2) {
     return (v1.val[0] <= v2.val[0]);
 }
 
 /**
- * This function is used in the sort() call so we can sort the vector containing the lines after the Y of the first point
+ * This function is used in the sort() call so we can sort DESCENDING the vector containing the lines after the X of the first point
  * @param v1 The first line
  * @param v2 The second line
  * @return returns if the X of the first point is smaller than the second lines first point X
  */
-bool comparisonFirstPointY(Vec4i v1, Vec4i v2) {
+bool comparisonFirstPointXDesc(Vec4i v1, Vec4i v2) {
+    return (v1.val[0] >= v2.val[0]);
+}
+
+/**
+ * This function is used in the sort() call so we can sort ASCENDING the vector containing the lines after the Y of the first point
+ * @param v1 The first line
+ * @param v2 The second line
+ * @return returns if the X of the first point is smaller than the second lines first point X
+ */
+bool comparisonFirstPointYAsc(Vec4i v1, Vec4i v2) {
     return (v1.val[1] <= v2.val[1]);
+}
+
+/**
+ * This function is used in the sort() call so we can sort DESCENDING the vector containing the lines after the Y of the first point
+ * @param v1 The first line
+ * @param v2 The second line
+ * @return returns if the X of the first point is smaller than the second lines first point X
+ */
+bool comparisonFirstPointYDesc(Vec4i v1, Vec4i v2) {
+    return (v1.val[1] >= v2.val[1]);
+}
+
+/**
+ * This function returns true if P1's X is smaller & Y is greater than P2's X and Y
+ * @param v1 Point line 1
+ * @param v2 Point line 2
+ * @return true if P1's X is smaller & Y is greater than P2's X and Y false otherwise
+ */
+bool comparisonFirstPointXLowYHigh(Vec4i v1, Vec4i v2) {
+    return (v1.val[0] <= v2.val[0] && v1.val[1] >= v2.val[1]);
+}
+
+/**
+ * This function returns true if P1's X & Y is greater than P2's X and Y
+ * @param v1 Point line 1
+ * @param v2 Point line 2
+ * @return true if P1's X is smaller & Y is greater than P2's X and Y false otherwise
+ */
+bool comparisonFirstPointXHighYHigh(Vec4i v1, Vec4i v2) {
+    return (v1.val[0] <= v2.val[0] && v1.val[1] <= v2.val[1]);
 }
 
 /**
@@ -166,8 +205,8 @@ bool comparisonFirstPointY(Vec4i v1, Vec4i v2) {
  * @param v2 A vector containing 4 points of a line, we take only the first point of the line
  * @return true if the X and Y of the points are equal
  */
-bool uniquePointComparison(Vec4i v1,Vec4i v2){
-    return v1.val[0] == v2.val[0] && v1.val[1]==v2.val[1];
+bool uniquePointComparison(Vec4i v1, Vec4i v2) {
+    return v1.val[0] == v2.val[0] && v1.val[1] == v2.val[1];
 }
 
 /**
@@ -179,13 +218,14 @@ bool uniquePointComparison(Vec4i v1,Vec4i v2){
  * @param src The vectorContaining the lines
  * @param comparisonType Type of comparison TODO:
  */
-void orderAndRemoveLines(vector<Vec4i> &lines, int comparisonType){
+void orderAndRemoveLines(vector<Vec4i> &lines, int comparisonType) {
     for (size_t i = 0; i < lines.size(); i++) {
         Vec4i l = lines[i];
         double line1Size = norm(Point(l[2], l[3]) - Point(l[0], l[1]));
         for (size_t j = 0; j < lines.size(); j++) {
             Vec4i l2 = lines[j];
-            if ((abs(l[0] - l2[0]) <= 15 && abs(l[1] - l2[1]) <= 10)||(abs(l[2] - l2[2]) <= 15 && abs(l[3] - l2[3]) <= 10) ) {
+            if ((abs(l[0] - l2[0]) <= 15 && abs(l[1] - l2[1]) <= 10) ||
+                (abs(l[2] - l2[2]) <= 15 && abs(l[3] - l2[3]) <= 10)) {
                 double line2Size = norm(Point(l2[2], l2[3]) - Point(l2[0], l2[1]));
                 if (line1Size < line2Size) {
                     lines[i] = lines[j];
@@ -196,13 +236,99 @@ void orderAndRemoveLines(vector<Vec4i> &lines, int comparisonType){
         }
     }
     // This will be used so we can remove the duplicates
-    sort(lines.begin(), lines.end(), comparisonFirstPointX);
-    lines.erase(unique(lines.begin(), lines.end(),uniquePointComparison), lines.end());
+    sort(lines.begin(), lines.end(), comparisonFirstPointXAsc);
+    lines.erase(unique(lines.begin(), lines.end(), uniquePointComparison), lines.end());
+}
+
+/**
+ * This method should detect the intersection of 2 lines
+ * @param o1
+ * @param p1
+ * @param o2
+ * @param p2
+ * @param r
+ * @return
+ */
+// Finds the intersection of two lines, or returns false.
+// The lines are defined by (o1, p1) and (o2, p2).
+bool intersection(Point2f o1, Point2f p1, Point2f o2, Point2f p2,
+                  Point2f &r) {
+    Point2f x = o2 - o1;
+    Point2f d1 = p1 - o1;
+    Point2f d2 = p2 - o2;
+
+    float cross = d1.x * d2.y - d1.y * d2.x;
+    if (abs(cross) < /*EPS*/1e-8)
+        return false;
+
+    double t1 = (x.x * d2.y - x.y * d2.x) / cross;
+    r = o1 + d1 * t1;
+    return true;
+}
+
+// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines
+// intersect the intersection point may be stored in the floats i_x and i_y.
+char get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y,
+                           float p2_x, float p2_y, float p3_x, float p3_y, float *i_x, float *i_y) {
+    float s1_x, s1_y, s2_x, s2_y;
+    s1_x = p1_x - p0_x;
+    s1_y = p1_y - p0_y;
+    s2_x = p3_x - p2_x;
+    s2_y = p3_y - p2_y;
+
+    float s, t;
+    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+    t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+        // Collision detected
+        if (i_x != NULL)
+            *i_x = p0_x + (t * s1_x);
+        if (i_y != NULL)
+            *i_y = p0_y + (t * s1_y);
+        return 1;
+    }
+
+    return 0; // No collision
+}
+
+void findSquareCoordinates(vector<Vec4i> &v1, vector<Vec4i> &v2) {
+    vector<Vec4i> positiveAngleLines(v1);
+    vector<Vec4i> negativeAngleLines(v2);
+
+    //We sorted after the Y value ASCENDING for the / lines
+    sort(positiveAngleLines.begin(), positiveAngleLines.end(), comparisonFirstPointYAsc);
+
+    //We sorted after the X value DESCENDING for the \ lines in order to take the first value out (There are others ways but lets stick to this one)
+    sort(negativeAngleLines.begin(), negativeAngleLines.end(), comparisonFirstPointXDesc);
+
+    Vec4i fistLinePositive, secondLinePositive, firstLineNegative, secondLineNegative;
+
+    //Get the bottom margin of the table and remove it from the vector
+    fistLinePositive = positiveAngleLines[positiveAngleLines.size() - 1];
+    positiveAngleLines.pop_back();
+
+    //Get the left margin of the table and remove it from the vector
+    firstLineNegative = negativeAngleLines[negativeAngleLines.size() - 1];
+    negativeAngleLines.pop_back();
+
+    //We sorted after the Y and X with the highest value ASCENDING for the / lines to get the one before the last line
+    sort(positiveAngleLines.begin(), positiveAngleLines.end(), comparisonFirstPointXHighYHigh);
+
+    //We sorted after the Y highest and X lowest value ASCENDING for the \ lines to get the one before the last line
+    sort(negativeAngleLines.begin(), negativeAngleLines.end(), comparisonFirstPointXLowYHigh);
+
+    secondLinePositive = positiveAngleLines[positiveAngleLines.size() - 1];
+    positiveAngleLines.pop_back();
+
+    secondLineNegative = negativeAngleLines[negativeAngleLines.size() - 1];
+    negativeAngleLines.pop_back();
+
 }
 
 /**
  *
- * this function uses probabilistic hough lines to identify all the lines in the image
+ * This function uses probabilistic Hough lines to identify all the lines in the image
  *
  * @param src The mat from where we will get the lines
  * @param gr The mat where we will write the lines for viewing
@@ -251,13 +377,13 @@ void houghLines(cv::Mat &src, cv::Mat &gr) {
                 linesAngle2.push_back(l);
             }
         }
-//        std::sort(linesAngle1.begin(), linesAngle1.end(), comparisonFirstPointX);
-//        std::sort(linesAngle2.begin(), linesAngle2.end(), comparisonFirstPointY);
+//        std::sort(linesAngle1.begin(), linesAngle1.end(), comparisonFirstPointXAsc);
+//        std::sort(linesAngle2.begin(), linesAngle2.end(), comparisonFirstPointYAsc);
         printf("Lines found with angle 2:%d and with -2 : %d \n", linesAngle1.size(), linesAngle2.size());
     }
 
-    orderAndRemoveLines(linesAngle1,1);
-    orderAndRemoveLines(linesAngle2,2);
+    orderAndRemoveLines(linesAngle1, 1);
+    orderAndRemoveLines(linesAngle2, 2);
 
     Mat oneTypeOfLines1, oneTypeOfLines2;
     oneTypeOfLines1 = gr.clone();
@@ -270,26 +396,39 @@ void houghLines(cv::Mat &src, cv::Mat &gr) {
         cvtColor(detected_edges, externalMat, CV_GRAY2BGR);
         line(externalMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
         //Create jpg files with the current line and the coordinates of its extremities
-        putTextIntoImage("../images/result/angle2",externalMat, 1, l[0], l[1], i);
-        putTextIntoImage("../images/result/angle2",externalMat, 2, l[2], l[3], i);
+        putTextIntoImage("../images/result/angle2", externalMat, 1, l[0], l[1], i);
+        putTextIntoImage("../images/result/angle2", externalMat, 2, l[2], l[3], i);
         printf("%d) Line P(%d,%d) P(%d,%d) slope %lf length %lf \n", i, l[0], l[1], l[2], l[3],
                (l[3] - l[1] / (double) (l[2] - l[0])), norm(Point(l[2], l[3]) - Point(l[0], l[1])));
+    }
+    if (linesAngle2.size() > 10) {
+        Vec4i l6 = linesAngle1[8];
+        Vec4i l8 = linesAngle1[6]; // this is reference slope
+        float *rx, *ry;
+        printf(" Line P(%d,%d) P(%d,%d) slope %lf \n", l6[0], l6[1], l8[3], l8[2],
+               (l8[3] - l6[1] / (double) (l8[2] - l6[0])));
+//        printf("These lines intersect %b",
+//               get_line_intersection(l6[0], l6[1], l6[2], l6[3], l8[0], l8[1], l8[2], l8[3], rx, ry));
     }
     imshow("oneTypeOfLines1", oneTypeOfLines1);
 
-    for (size_t i = 0; i < linesAngle2.size(); i++) {
-        Vec4i l = linesAngle2[i];
-        line(oneTypeOfLines2, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
-        Mat externalMat;
-        cvtColor(detected_edges, externalMat, CV_GRAY2BGR);
-        line(externalMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
-        //Create jpg files with the current line and the coordinates of its extremities
-        putTextIntoImage("../images/result/anglen2",externalMat, 1, l[0], l[1], i);
-        putTextIntoImage("../images/result/anglen2",externalMat, 2, l[2], l[3], i);
-        printf("%d) Line P(%d,%d) P(%d,%d) slope %lf length %lf \n", i, l[0], l[1], l[2], l[3],
-               (l[3] - l[1] / (double) (l[2] - l[0])), norm(Point(l[2], l[3]) - Point(l[0], l[1])));
-    }
-    imshow("oneTypeOfLines2", oneTypeOfLines2);
+//    for (size_t i = 0; i < linesAngle2.size(); i++) {
+//        Vec4i l = linesAngle2[i];
+//        line(oneTypeOfLines2, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+//        Mat externalMat;
+//        cvtColor(detected_edges, externalMat, CV_GRAY2BGR);
+//        line(externalMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+//        //Create jpg files with the current line and the coordinates of its extremities
+//        putTextIntoImage("../images/result/anglen2", externalMat, 1, l[0], l[1], i);
+//        putTextIntoImage("../images/result/anglen2", externalMat, 2, l[2], l[3], i);
+//        printf("%d) Line P(%d,%d) P(%d,%d) slope %lf length %lf \n", i, l[0], l[1], l[2], l[3],
+//               (l[3] - l[1] / (double) (l[2] - l[0])), norm(Point(l[2], l[3]) - Point(l[0], l[1])));
+//    }
+//    imshow("oneTypeOfLines2", oneTypeOfLines2);
+
+//    if (linesAngle1.size() > 0 && linesAngle2.size() > 0) {
+//        findSquareCoordinates(linesAngle1, linesAngle2);
+//    }
 
     // THIS WILL PRINT ALL THE LINES ON THE IMAGE
 //    for (size_t i = 0; i < lines.size(); i++) {
